136205f3ffe7542d8e8519e8559cf6d6f9a97c13
Jangwoo Park <p.jangwoo@gmail>
2024-03-14 18:26:33 +0900

    03월 14일 - 저녁

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import './Editor.css'; 
+import {emotionList} from '../utils'; 
+import EmotionItem from './EmotionItem';
 
-function Editor(props) {
+function Editor({initData, onSubmit}) {
+
+    const handleChangeEmotion = () => {
+        console.log('이벤트 전송 잘됨 (Editor : handleChangeEmotion 함수')
+    }
+
     return (
-        <div>
-            
+        <div className="Editor">
+            <h4> 오늘의 날짜 </h4>
+            <div className="input_wrapper">
+                <input type="date" /> 
+            </div>
+
+            {/* 이모티지를 출력 하는 블락 */}
+            <div className="editor_section">
+                <h4> 오늘의 감정</h4>
+                <div className="input_wrapper emotion_list_wrapper">
+                   {
+                   emotionList.map( (it) => (
+                        <EmotionItem key={it.id} {...it} 
+                            onClick={handleChangeEmotion}  
+                            isSelected={true}
+                            />
+                   ) )} 
+                </div>
+            </div>
+            <div className="editor_section"></div>
+            <div className="editor_section bottom_section"></div>
         </div>
     );
 }
